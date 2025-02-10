afdcea48de4e576efee37a57d403a6487939e6f7
davidayodele64@gmail.com <davidayodele64@gmail.com>
2025-02-09 23:30:13 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1" 
+}
+
+#create S3 bucket
+resource "aws_s3_bucket" "resumebucket" {
+  bucket = "resume-bucket-davida"
+}
+
+
+resource "aws_s3_bucket_versioning" "resumebucketversioning" {
+  bucket = aws_s3_bucket.resumebucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable static website hosting
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.resumebucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+
